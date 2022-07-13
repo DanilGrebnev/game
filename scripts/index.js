4878a0fae558432b1acab0eb8c8ac53e151a323a
Danil Grebnev <t902hda@mail.ru>
2022-07-13 16:32:21 +0300

    оптимизация

--- scripts/index.js
+++ scripts/index.js
@@ -6,25 +6,26 @@ import { data as d } from './var.js'
 window.addEventListener('load', main)
 
 function main() {
-    let setId;
     //Число, получаемое с формы
     let value;
     let togglePlay = true
     let multiVal = {
         counter: 1,
-        pl1: 0,
-        pl2: 0,
+        step1: 0,
+        step2: 0,
         vl1: [],
         vl2: [],
+        data1: Number(''),
+        data2: Number(''),
     };
     //Количество попыток
     let count = 0;
-    //генерация случайного числа
+    //Приходит из уровня сложности
     let number;
+    //генерация случайного числа
     let rand = random(number)
     //Массив с введёнными значениями
     let a = []
-
 
     //Сброс данных 
     const reset = () => {
@@ -37,15 +38,105 @@ function main() {
         d.attempNmb.innerText = a;
         d.al.innerText = '';
         d.surrender.innerText = 'Назад';
-        // multiVal = {
-        //     counter: 1,
-        //     pl1: 0,
-        //     pl2: 0,
-        //     vl1: [],
-        //     vl2: [],
-        // }
     }
 
+    //ПРОВЕРКА ЧИСЛА
+    d.form.addEventListener('submit', e => {
+        e.preventDefault()
+        if (!togglePlay) {
+            examination(d.input.value)
+        } else if (togglePlay) {
+            conditionMulti()
+            switch (multiVal.counter) {
+                case 1:
+                    examination(multiVal.data1)
+                    break
+                case 2:
+                    examination(multiVal.data2)
+                    break
+            }
+        }
+    })
+
+    //Функция проверки числа
+    function examination(el) {
+        //Проверка на нежелаемый тип данных
+        switch (el) {
+            case null:
+            case ' ':
+            case undefined:
+                return
+        }
+        if (rand > el) {
+            d.al.innerText = 'Число, которое я загадал, больше твоего'
+        } else if (rand < el) {
+            d.al.innerText = 'Число, которое я загадал, меньше твоего'
+        } else if (rand === el) {
+            showWin(d.game, d.win, rand, count)
+        }
+        count++
+    }
+
+    function conditionMulti() {
+        let { step1, step2, vl1, vl2, data1, data2, counter } = multiVal;
+        let input = +d.input.value
+        switch (multiVal.counter) {
+            case 1: {
+                step1++
+                data1 = input
+                counter++
+                vl1 = [...multiVal.vl1, multiVal.data1]
+                break;
+            }
+            case 2: {
+                step2++
+                data2 = input
+                vl2 = [...multiVal.vl2, +d.input.value]
+                counter = 1
+                break
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Инкремент значения в поле
+    d.inc.addEventListener('click', () => {
+        let val = d.input.value;
+        val++
+        d.input.value = +val
+        if (!togglePlay) value = val
+    })
+    d.dec.addEventListener('click', () => {
+        let val = +d.input.value
+        val--
+        d.input.value = +val
+        if (!togglePlay) value = val
+    })
+    //==============================================================================================
+    //Выбор сложности
+    d.ul.addEventListener('click', e => {
+        if (e.target?.dataset?.difficult) {
+            number = e.target.dataset.difficult
+            d.difInf.forEach(el => el.innerText =
+                `${e.target.innerText} (0 - ${e.target.dataset.difficult})`)
+            rand = random(number)
+        }
+    })
     //Навигацию по меню
     document.addEventListener('click', e => {
         const el = e.target
@@ -69,112 +160,6 @@ function main() {
             togglePlay = true
         }
     })
-
-    //Выбор сложности
-    d.ul.addEventListener('click', e => {
-        if (e.target?.dataset?.difficult) {
-            number = e.target.dataset.difficult
-            d.difInf.forEach(el => el.innerText =
-                `${e.target.innerText} (0 - ${e.target.dataset.difficult})`)
-            rand = random(number)
-        }
-    })
-
-    d.input.addEventListener('input', (e) => {
-        if (!togglePlay) {
-            value = e.target.value
-
-        } else if (togglePlay) {
-
-        }
-
-    })
-
-    //Прибавить или отнять единицу от числа;
-    d.inc.addEventListener('click', () => {
-        let val = d.input.value;
-        val++
-        d.input.value = +val
-        if (!togglePlay) {
-            value = val
-        } else {
-            // multiVal = val;
-        }
-    })
-    d.dec.addEventListener('click', () => {
-        let val = +d.input.value
-        val--
-        d.input.value = +val
-        if (!togglePlay) {
-            value = val
-        } else {
-            multiVal = val;
-        }
-
-    })
-
-    //ПРОВЕРКА ЧИСЛА
-    d.form.addEventListener('submit', e => {
-        e.preventDefault()
-        if (!togglePlay) {
-            if (value === null || value === ' ' || value === undefined) return
-            a = [...a, value]
-            if (rand > value) {
-                d.al.innerText = 'Число, которое я загадал, больше твоего'
-            } else if (rand < value) {
-                d.al.innerText = 'Число, которое я загадал, меньше твоего'
-            } else if (rand === +value) {
-                count++
-                d.attempNmb.innerText = a
-                showWin(d.game, d.win, rand, count)
-                reset()
-                return
-            }
-            d.attemp.innerText = `Вы ввели: `;
-            d.attempNmb.innerText = a;
-            count++;
-            if (count > 0) { d.surrender.innerText = 'Сдаться' }
-            return
-        } else if (togglePlay) {
-            conditionMulti()
-            console.log(rand)
-        }
-
-    })
-
-    function conditionMulti() {
-        switch (multiVal.counter) {
-            case 1: {
-                multiVal.pl1++
-                multiVal.counter++
-                multiVal.vl1 = [...multiVal.vl1, +d.input.value]
-                break;
-            }
-            case 2: {
-                multiVal.pl2++
-                multiVal.vl2 = [...multiVal.vl2, +d.input.value]
-                multiVal.counter = 1
-                break
-            }
-        }
-        if (multiVal.vl1.includes(rand)) {
-            showWin(d.game, d.win, rand, multiVal.pl1)
-            add(d.win, 'active')
-        } else if (multiVal.vl2.includes(rand)) {
-            showWin(d.game, d.win, rand, multiVal.pl2)
-        }
-
-        console.log(
-            `
-                multiVal.counter: ${multiVal.counter}
-                multiVal.pl1: ${multiVal.vl1}
-                multiVal.pl2: ${multiVal.vl2}
-                `)
-
-
-
-    }
-
     //Открытие и закрытие списка сложности
     d.difficult_p.addEventListener('click', () => {
         if (d.difficult_p.classList.contains('active')) {
@@ -185,6 +170,7 @@ function main() {
         toggle(d.ul, 'active')
     });
     //Задержки для анимации меню настроек
+    let setId;
     d.ul.addEventListener('click', e => {
         const v = e.target;
         if (v.dataset.difficult) {
@@ -220,9 +206,6 @@ function main() {
             backToMenu()
         }
     })
-
-
-
 
     //Закрытие алерта победы
     d.win.addEventListener('click', backToMenu)
